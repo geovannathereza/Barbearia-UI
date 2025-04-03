ad91dd539812fce7e488f1489e9fc88e03488bf3
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:20:31 -0300

    Update list-clients.component.ts

--- src/app/clients/list-clients/list-clients.component.ts
+++ src/app/clients/list-clients/list-clients.component.ts
@@ -20,9 +20,7 @@ import { ClientTableComponent } from '../components/client-table/client-table.co
   ]
 })
 export class ListClientsComponent implements OnInit, OnDestroy {
-
   private httpSubscriptions: Subscription[] = []
-
   clients: ClientModelTable[] = []
 
   constructor(
@@ -31,21 +29,14 @@ export class ListClientsComponent implements OnInit, OnDestroy {
     private readonly router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.httpSubscriptions.push(this.httpService.list().subscribe(data => this.clients = data))
-  }
-  ngOnDestroy(): void {
-    this.httpSubscriptions.forEach(s => s.unsubscribe())
-  }
+  ngOnInit(): void { this.httpSubscriptions.push(this.httpService.list().subscribe(data => this.clients = data)) }
+  ngOnDestroy(): void { this.httpSubscriptions.forEach(s => s.unsubscribe()) }
 
-  update(client: ClientModelTable) {
-    this.router.navigate(['clients/edit-client', client.id])
-  }
+  update(client: ClientModelTable) { this.router.navigate(['clients/edit-client', client.id]) }
 
   delete(client: ClientModelTable) {
     this.httpSubscriptions.push(
       this.httpService.delete(client.id).subscribe(_ => this.snackBarManager.show(`O cliente ${client.name} foi excluido com sucesso`))
     )
   }
-
 }
